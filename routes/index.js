72360fe008755f55702c532b1764d30384b29634
Liudmyla Iefremova <liudmyla.iefremova@gmail.com>
2017-10-13 11:58:34 +0200

    added Apikey to router for deployment

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,11 @@ const Organization = require('../models/organization');
 router.get('/', function(req, res, next) {
 	Organization.find((error, organizations) => {
 		if (error) { next(error); }
-    else {
-      res.render('index', { organizations });
-    }
+    	else {
+      		res.render('index', { 
+      		apiKey: "AIzaSyAADIzhXOJbzOfhryet6qTIr9ZpMH26rq4",
+      		organizations });
+    	}
 	})
   //res.render('index', { title: 'DynamicMap' });
 });
